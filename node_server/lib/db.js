cfe8b8be27f577562ffe71ada98b827c79ecac23
iliazodo <zodozdm@gmail.com>
2026-01-10 20:48:55 -0800

    Routes & Database connection initialized

---
+++ node_server/lib/db.js
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+
+export const connectDB = async () => {
+    try {
+        const conn = await mongoose.connect(process.env.MONGODB_URI);
+        console.log("MongoDB database connected successfully");
+    } catch (error) {
+        console.log("Unable to connect to MongoDB database: ", error);
+    }
+}
--- node_server/server.js
+++ node_server/server.js
@@ -1,11 +1,24 @@
 import express from "express";
 import dotenv from "dotenv";
 
+// Routes
+import userRoutes from "./routes/user.route.js"
+import authRoutes from "./routes/auth.route.js"
+import saveRoutes from "./routes/save.route.js"
+
+// Database
+import { connectDB } from "./lib/db.js";
+
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT;
 
+app.use("/api/ussers" , userRoutes);
+app.use("/api/auth" , authRoutes);
+app.use("/api/save" , saveRoutes);
+
 app.listen(PORT , () => {
     console.log("server is running on port " + PORT);
+    connectDB();
 })
